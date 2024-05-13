23dba3b86f9afb748f898e68ee911da027615411
Weileeeee <leongweilee49@gmail.com>
2024-05-14 00:45:05 +0800

    added routes for the website

--- Test/Flask_app_web_tutorial/website/__init__.py
+++ Test/Flask_app_web_tutorial/website/__init__.py
@@ -5,6 +5,13 @@ def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'HELLO'
 
+    #routes
+    from .views import views
+    from .auth import auth
+
+    app.register_blueprint(views, url_prefix='/')
+    app.register_blueprint(auth, url_prefix='/auth')
+
     return app 
 
 
