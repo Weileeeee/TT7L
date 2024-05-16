a3933ac1d9a61e576873778024adc869f19bb1f4
Weileeeee <leongweilee49@gmail.com>
2024-05-17 00:42:53 +0800

    added connected function in testing

--- Test/Flask_app_web_tutorial/website/auth.py
+++ Test/Flask_app_web_tutorial/website/auth.py
@@ -1,11 +1,11 @@
-from flask import Blueprint
+from flask import Blueprint,render_template
 
 #defining blueprint
 auth = Blueprint('auth', __name__)
 
 @auth.route('/login')
 def login():
-    return"<p>login<p>"
+    return render_template("login.html")
 
 @auth.route('/logout')
 def logout():
@@ -13,4 +13,4 @@ def logout():
 
 @auth.route('/sign-up')
 def sign_up():
-    return"<p>Sign up<p>"
+    return render_template("sign_up.html")
