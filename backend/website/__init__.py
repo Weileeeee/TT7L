cfe5fe092bfb8087792055cef79d5ef37564ae48
Weileeeee <leongweilee49@gmail.com>
2024-05-24 10:49:36 +0800

    added flashing function to tell user requirements

--- backend/website/__init__.py
+++ backend/website/__init__.py
@@ -1,9 +1,19 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+#sql setup
+db  = SQLAlchemy()
+DB_NAME ="database.db"
+
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'HELLO'
+    #storing my sql database
+    app.config['SQLALCHEMY_DATABASE_URL']=f'sqlite:///{DB_NAME}'
+    db.init_app(app)
 
+    
     #import blueprints
     from .views import views
     from .auth import auth
--- backend/website/auth.py
+++ backend/website/auth.py
@@ -1,9 +1,9 @@
-from flask import Blueprint,render_template
+from flask import Blueprint,render_template,request,flash
 
 #defining blueprint
 auth = Blueprint('auth',__name__)
 
-@auth.route('/login')
+@auth.route('/login',methods=['GET', 'POST'])
 def login():
     return render_template ("login.html")
 
@@ -11,7 +11,23 @@ def login():
 def logout():
     return"<h1>logout<h1>"
 
-@auth.route('/Signup')
+@auth.route('/Signup',methods=['POST', 'GET'])
 def signup():
+    if request.method == 'POST':
+        email  = request.form.get('email')
+        firstname = request.form.get('firstname')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+        #check for signup requirements
+        if len(email)< 4:
+            flash('Email must be greater than 4 characters.', category='error')
+        elif len(firstname) < 2:
+            flash('Email must be greater than 1 characters.',category='error')
+        elif password1 <7:
+            flash('Password must be 7 characters', category ='error')
+        elif password2 != password1:
+            flash("passwprd don\'t match." ,category ='error')
+        else:
+            flash('Account created! ', category='success')
     return render_template("Signup.html")
 
